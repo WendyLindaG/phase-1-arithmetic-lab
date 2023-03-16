dc9f12b5870b6ecce9bd33401766b07713d90acf
Wendy Linda Gakii <wendylinda2017@gmail.com>
2023-03-16 10:36:33 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; // multiply num1 and num2
+console.log(multiply); // output: 62let 
+
+let random = 4 ;
+Math.random() * 10 + 1; // generate a random integer between 1 and 100
+
+
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4; // calculate remainder of num3 divided by num4
+
+
+let max = Math.max (4, 12, 5, 13, 18, 20, 15);
+
+
+
